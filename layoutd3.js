dd3a61fd12d2e09bfafee7be00427c8ab60b3f93
daphshez <daphna@shezaf.com>
2015-10-30 19:09:31 +0000

    cooler implementation of the onclick function for squares

--- layoutd3.js
+++ layoutd3.js
@@ -15,43 +15,48 @@ $(document).ready(function () {
     svg.append('rect').attr('height', '100%').attr('width', '100%').attr('fill', 'black');
 
 
-    function id2x (id) {
+    function id2x(id) {
         return Math.floor(id / gridSize);
     }
+
     function id2y(id) {
         return id % gridSize;
     }
 
-    var rectXY = d3.range(0, gridSize * gridSize).map(function(d){
-        return {'x': id2x(d), 'y': id2y(d)};
+    var rectXY = d3.range(0, gridSize * gridSize).map(function (d) {
+        return {'x': id2x(d), 'y': id2y(d), 'color': 'white'};
     });
 
-    svg.selectAll('rect.square')
+
+    var rects = svg.selectAll('rect.square')
         .data(rectXY)
         .enter()
         .append('rect')
-        .attr('id', function(d) { return 'rect-' + d.x + '-' + d.y; })
+        .attr('id', function (d) {
+            return 'rect-' + d.x + '-' + d.y;
+        })
         .attr('class', 'square')
-        .attr('x', function(d) { return stroke + d.x * (stroke + squareSize)   })
-        .attr('y', function(d) { return stroke + d.y * (stroke + squareSize)   })
+        .attr('x', function (d) {
+            return stroke + d.x * (stroke + squareSize)
+        })
+        .attr('y', function (d) {
+            return stroke + d.y * (stroke + squareSize)
+        })
         .attr('width', squareSize).attr('height', squareSize)
         .attr('stroke-width', '0')
         .attr('fill', 'white')
-        .on('click', function(d) {
-            var dis = d3.select(this);
-            var currentColor = dis.attr('fill');
-            var newColor = currentColor == 'white' ? 'black' : 'white';
-            dis.attr('fill', newColor);
-            if (symmetry) {
-                var otherX = gridSize - 1 - d.x;
-                var otherY = gridSize - 1 - d.y;
-                console.log(otherX, otherY);
-                console.log(d3.select(".rect-" + otherX + "-" + d.y));
-                d3.select("#rect-" + otherX + "-" + d.y).attr('fill', newColor);
-                d3.select("#rect-" + otherX + "-" + otherY).attr('fill', newColor);
-                d3.select("#rect-" + d.x + "-" + otherY).attr('fill', newColor);
-
-            }
+        .on('click', function (d) {
+            var newColor = (d.color == "white") ? "black" : "white";
+            rects.filter(function (d1) {
+                if ((d1.x == d.x || d1.x == (gridSize - 1 - d.x)) && (d1.y == d.y || d1.y == (gridSize - 1 - d.y))) {
+                    d1.color = newColor;
+                    return true;
+                }
+                return false;
+            }).attr('fill', function (d) {
+                return d.color;
+            })
         });
+
 
 });
